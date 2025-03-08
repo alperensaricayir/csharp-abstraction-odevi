7f7c8dd36425b4d662f94fe37d80f0d382e6a163
Alperen <alperensarcayr@gmail.com>
2025-03-08 22:24:46 +0300

    Add files via upload

---
+++ odev-2-abstraction-alperen/abstraction-odev-2-alperen/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public interface IMaasHesaplama
+{
+    int MaasHesapla();
+}
+
+public abstract class Calisan : IMaasHesaplama
+{
+    public abstract int MaasHesapla();
+}
+
+public class GenelMudur : Calisan
+{
+    public override int MaasHesapla()
+    {
+        return 80000;
+    }
+}
+
+public class Mudur : Calisan
+{
+    public override int MaasHesapla()
+    {
+        return 60000;
+    }
+}
+
+public class Programci : Calisan
+{
+    public override int MaasHesapla()
+    {
+        return 40000;
+    }
+}
+
+public class Stajyer : Calisan
+{
+    public override int MaasHesapla()
+    {
+        return 5000;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Calisan[] calisanlar = { new GenelMudur(), new Mudur(), new Programci(), new Stajyer() };
+        int toplamMaas = 0;
+
+        foreach (var calisan in calisanlar)
+        {
+            toplamMaas += calisan.MaasHesapla();
+        }
+
+        Console.WriteLine("Toplam maaş: " + toplamMaas);
+        Console.ReadKey(); // Konsolun kapanmasını engeller.
+    }
+}
