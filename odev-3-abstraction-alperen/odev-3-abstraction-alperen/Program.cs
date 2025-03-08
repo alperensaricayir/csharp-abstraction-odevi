03e9f6d49989ea206b689be732c07e07a648e8ad
Alperen <alperensarcayr@gmail.com>
2025-03-08 22:25:39 +0300

    Add files via upload

---
+++ odev-3-abstraction-alperen/odev-3-abstraction-alperen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public interface IYakitTuketimi
+{
+    double YakitTuketimiHesapla();
+}
+
+public abstract class Araba : IYakitTuketimi
+{
+    public abstract double YakitTuketimiHesapla();
+}
+
+public class BMW : Araba
+{
+    public override double YakitTuketimiHesapla()
+    {
+        return 80.0;
+    }
+}
+
+public class Mercedes : Araba
+{
+    public override double YakitTuketimiHesapla()
+    {
+        return 70.0;
+    }
+}
+
+public class Porsche : Araba
+{
+    public override double YakitTuketimiHesapla()
+    {
+        return 60.0;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Araba[] arabalar = { new BMW(), new Mercedes(), new Porsche() };
+        double toplamYakit = 0;
+
+        foreach (var araba in arabalar)
+        {
+            toplamYakit += araba.YakitTuketimiHesapla();
+        }
+
+        Console.WriteLine("Toplam yakıt tüketimi: " + toplamYakit + " L");
+        Console.ReadKey(); // Konsolun kapanmasını engeller.
+    }
+}
